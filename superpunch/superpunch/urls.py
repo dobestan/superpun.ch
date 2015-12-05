1eb28d993837abf8241855d154b2e9b28fa8a4b3
dobestan <dobestan@gmail.com>
2015-12-05 14:36:37 +0900

    add Facebook Redirect Views including redirect to facebook page, and message

--- superpunch/superpunch/urls.py
+++ superpunch/superpunch/urls.py
@@ -2,13 +2,19 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 from tidbits.views import *
+from superpunch.views import *
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^', include([
+        url(r'^facebook/$', FacebookPageRedirectView.as_view(), name='page'),
+        url(r'^facebook/message/$', FacebookMessageRedirectView.as_view(), name='message'),
+    ], namespace='facebook')),
+
+    url(r'^', include([
         url(r'^$', TidbitsListView.as_view(), name='list'),
         url(r'^(?P<slug>\w+)/$', TidbitsDetailView.as_view(), name='detail'),
-    ], namespace='tidbits', app_name='tidbits'))
+    ], namespace='tidbits', app_name='tidbits')),
 ]
---
+++ superpunch/superpunch/views/facebook.py
@@ -0,0 +1,19 @@
+from django.views.generic.base import RedirectView
+
+
+class FacebookPageRedirectView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        facebook_page_slug = 'superpun.ch'
+        return "https://www.facebook.com/{facebook_page_slug}".format(
+            facebook_page_slug=facebook_page_slug,
+        )
+
+
+class FacebookMessageRedirectView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        facebook_page_id = '652793861490830'
+        return "https://m.facebook.com/messages/thread/{facebook_page_id}".format(
+            facebook_page_id=facebook_page_id,
+        )
