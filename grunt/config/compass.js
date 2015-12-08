3253039221d809010dd4e9dd24a32a190185b1aa
dobestan <dobestan@gmail.com>
2015-12-08 12:52:04 +0900

    init static css management via grunt, compass

---
+++ grunt/config/compass.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dist: {
+    options: {
+      sassDir: './superpunch/superpunch/static/scss/',
+      cssDir: './dist/static/css/',
+
+      sourcemap: true,
+    }
+  }
+}
--- superpunch/superpunch/settings/partials/application.py
+++ superpunch/superpunch/settings/partials/application.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'pipeline',
+
     'tidbits',
 )
 
